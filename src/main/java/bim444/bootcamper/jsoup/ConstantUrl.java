a944d3eb89ccb70a126fe41e45a5b536aa9de583
Ahmet Dayi <ahmet_dayi@eskisehir.edu.tr>
2024-03-31 18:50:07 +0300

    todo

--- src/main/java/bim444/bootcamper/jsoup/ConstantUrl.java
+++ src/main/java/bim444/bootcamper/jsoup/ConstantUrl.java
@@ -10,5 +10,5 @@ public class ConstantUrl {
     private static final String TECHCAREER_BASE_URL = "https://www.techcareer.net";
     public static final String TECHCAREER_BOOTCAMPS_URL = TECHCAREER_BASE_URL + "/bootcamp";
     public static final String TECHCAREER_USER_AGENT = "Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Mobile Safari/537.36";
-
+//todo userbootcamp tablosuna tarih ekleyip kullanicinin aylik istatiskleri gosterilebilir. bunu en son proje bittiginde zaman kalirsa ekle
 }
